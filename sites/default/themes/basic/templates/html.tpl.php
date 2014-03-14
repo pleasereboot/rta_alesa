684f67921431a6d105b74ddbc8e2edbba5e7ded7
pleasereboot <yannick.yyy@gmail.com>
2014-03-14 14:17:28 -0400

    init

--- sites/default/themes/basic/templates/html.tpl.php
+++ sites/default/themes/basic/templates/html.tpl.php
@@ -8,9 +8,9 @@
   <meta name="viewport" content="width=device-width">
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
-  <div id="skip">
+<!--   <div id="skip">
     <a href="#main-menu"><?php print t('Jump to Navigation'); ?></a>
-  </div>
+  </div> -->
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
